d1ce8e5ac8818e9d7ea772106b90da981da3055f
jamesbaroyofficial <146837518+jamesbaroyofficial@users.noreply.github.com>
2026-09-23 21:24:44 +0800

    Refactor autoseen module with improved functionality

--- scripts/cmds/autoseen.js
+++ scripts/cmds/autoseen.js
@@ -1,18 +1,47 @@
+const fs = require('fs-extra');
+const pathFile = __dirname + '/cache/txt/autoseen.txt';
+
 module.exports = {
-config: {
-name: "autoseen",
-author: "Aminul Sardar",
-role: 0,
-shortDescription: "Text to image",
-guide: "{pn} <query>",
-category: "midjorney"
+  config: {
+  name: "autoseen",
+  version: "1.0.0",
+  hasPermssion: 2,
+  author: "Jas",
+  shortDescription: {
+      en: "Turn on/off automatically seen when new messages are available"
+    },
+    longDescription: {
+      en: "Turn on/off automatically seen when new messages are available"
+    },
+  category: "Admin",
+  guide: {
+   en: "on/off",
 },
-onStart: async function ({ message, args }) {
-var prompt = args.join(" ");
-var imagine = "https://sandipapi.onrender"+ ".com/dalle";
-message.reply({
-body: prompt,
-attachment: await global.utils.getStreamFromURL(imagine)
-});
+  cooldowns: 5,
+},
+
+onChat: async ({ api, event, args }) => {
+if (!fs.existsSync(pathFile))
+   fs.writeFileSync(pathFile, 'false');
+   const isEnable = fs.readFileSync(pathFile, 'utf-8');
+   if (isEnable == 'true')
+     api.markAsReadAll(() => {});
+},
+
+onStart: async ({ api, event, args }) => {
+   try {
+     if (args[0] == 'on') {
+       fs.writeFileSync(pathFile, 'true');
+       api.sendMessage('The autoseen function is now enabled for new messages.', event.threadID, event.messageID);
+     } else if (args[0] == 'off') {
+       fs.writeFileSync(pathFile, 'false');
+       api.sendMessage('The autoseen function has been disabled for new messages.', event.threadID, event.messageID);
+     } else {
+       api.sendMessage('Incorrect syntax', event.threadID, event.messageID);
+     }
+   }
+   catch(e) {
+     console.log(e);
+   }
 }
 };
